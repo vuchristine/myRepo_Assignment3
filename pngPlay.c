86add59f75efad3191e6f478e6be5d4556fed4f5
vuchristine <vuchris@uw.edu>
2016-08-03 23:51:32 -0700

    Updating pngPlay.c
    Editing file with switch case

--- pngPlay.c
+++ pngPlay.c
@@ -1,8 +1,12 @@
 #include "pixutils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
   char *inputfile=0,*outputfile=0;
   float degrees=0, grayFlag=0;
+  int option = 0;
   
   //write the parser yourself or use a package like https://github.com/skeeto/optparse
   
@@ -17,6 +21,49 @@ int main(int argc, char *argv[]){
   if(grayFlag)pixMap_gray(p);
   pixMap_write(p,outputfile);
   pixMap_destroy(p);
+  
+  //while any option is i/o/r/g and not -1
+  while((option = getopt(argc, argv, "i:o:r:g")) != -1) {
+
+	  //switch cases for i/o/r/g
+	  switch(option) {
+
+		  //input file
+		  case 'i':
+			  inputfile = optarg;
+			  fprintf(stderr,"The input file is %s\n", inputfile);
+			  break;
+
+		  //output file
+		  case 'o':
+			  outputfile = optarg;
+			  fprintf(stderr,"The output file is %s\n", outputfile);
+			  //write out to output file
+			  pixMap_write(p, outputfile);
+			  break;
+
+		  //if user wants to rotate image
+		  case 'r':
+			  degrees = atof(optarg);
+			  fprintf(stderr,"The image is being rotated by %f degrees\n", degrees);
+			  //if degrees exist
+			  if(degrees) {
+				  //rotate image by this amount of degrees
+				  pixMap_rotate(p,degrees);
+			  }
+			  break;
+
+		  //if user wants to gray image
+		  case 'g':
+			  grayFlag = atof(optarg);
+			  fprintf(stderr,"The grayFlag is %f\n", grayFlag);
+			  //if grayFlag exists
+			  if(grayFlag) {
+				  //gray image
+				  pixMap_gray(p);
+			  }
+	  }
+  }
   
   return 0;
 }
